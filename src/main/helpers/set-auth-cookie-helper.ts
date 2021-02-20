bad88087a7ef51d8b18f70e7ec3546e4b4bbaaae
Daniel <danielgg0831@gmail.com>
2021-02-20 13:01:55 -0300

    refactor: change the cookie sameSite attr to none

--- src/main/helpers/set-auth-cookie-helper.ts
+++ src/main/helpers/set-auth-cookie-helper.ts
@@ -4,6 +4,7 @@ export const setAuthCookie = (res: Response, token: string): void => {
   res.cookie('d794$7dsa99_dsadsa978lbipe$sdspp', token, {
     maxAge: 1000 * 60 * 30, // expires in 30 minutes
     httpOnly: true,
-    sameSite: 'lax'
+    sameSite: 'none',
+    secure: true
   })
 }
